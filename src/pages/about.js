7b4b7287129c4dfee28b329d563cfefa0d67e3e7
Nozomi Ishii <non.ankt@gmail.com>
2020-03-21 05:12:24 +0900

    style success

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,23 +1,26 @@
 import React from "react"
 import Layout from "../components/Layout"
+import aboutStyles from "../styles/pages/about.module.scss"
 
 const About = () => (
-  <Layout className="about">
-    <h1>Who's Nozomi Ishii??</h1>
-    <a
-      href="https://twitter.com/nozomiishii_jp"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      TWITTER
-    </a>
-    <a
-      href="https://github.com/nozomiishii"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      GITHUB
-    </a>
+  <Layout>
+    <div className={aboutStyles.about}>
+      <h1>Who's Nozomi Ishii??</h1>
+      <a
+        href="https://twitter.com/nozomiishii_jp"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        TWITTER
+      </a>
+      <a
+        href="https://github.com/nozomiishii"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        GITHUB
+      </a>
+    </div>
   </Layout>
 )
 
