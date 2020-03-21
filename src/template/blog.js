0f79a423c89c0f30620f80f123c4688c42709f41
Nozomi Ishii <non.ankt@gmail.com>
2020-03-21 20:46:16 +0900

    resize the width

--- src/template/blog.js
+++ src/template/blog.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/Layout"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
 export const query = graphql`
   query($slug: String!) {
@@ -14,11 +15,26 @@ export const query = graphql`
   }
 `
 
-const Blog = props => (
-  <Layout>
-    <h1>{props.data.contentfulBlogPost.title}</h1>
-    <p>{props.data.contentfulBlogPost.date}</p>
-  </Layout>
-)
+const Blog = props => {
+  const options = {
+    renderNode: {
+      "embedded-asset-block": node => {
+        const alt = node.data.target.fields.title["en-US"]
+        const url = node.data.target.fields.file["en-US"].url
+        return <img src={url} alt={alt} />
+      },
+    },
+  }
+  return (
+    <Layout>
+      <h1>{props.data.contentfulBlogPost.title}</h1>
+      <p>{props.data.contentfulBlogPost.date}</p>
+      {documentToReactComponents(
+        props.data.contentfulBlogPost.body.json,
+        options
+      )}
+    </Layout>
+  )
+}
 
 export default Blog
