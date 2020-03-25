d379ecd0f164a63855de6544052728a691633e97
Nozomi Ishii <non.ankt@gmail.com>
2020-03-25 21:50:55 +0900

    about

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,13 +3,33 @@ import Layout from "../components/Layout";
 import aboutStyles from "../styles/pages/about.module.scss";
 import Head from "../components/Head";
 import LinkToSNS from "../components/LinkToSNS";
+import { useStaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
 
 const AboutPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      images: allFile(filter: { relativeDirectory: { eq: "images/about" } }) {
+        nodes {
+          id
+          name
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  `);
+  console.log(data);
+  console.log(data.images.nodes);
   return (
     <Layout>
       <Head title="ABOUT" />
       <div className={aboutStyles.about}>
         <h2>Who's Nozomi Ishii?</h2>
+        {/* <Img /> */}
         <LinkToSNS />
       </div>
       <div>privious webpage</div>
