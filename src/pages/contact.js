5da657ea0b1d3883583b953809336a8d573428b1
Nozomi Ishii <non.ankt@gmail.com>
2020-03-27 17:35:37 +0900

    test

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -98,6 +98,37 @@ const ContactPage = () => {
                 Send
               </Button>
             </Form>
+
+            <h2>test</h2>
+            <form name="contact2" method="POST" data-netlify="true">
+              <p>
+                <label>
+                  Your Name: <input type="text" name="name" />
+                </label>
+              </p>
+              <p>
+                <label>
+                  Your Email: <input type="email" name="email" />
+                </label>
+              </p>
+              <p>
+                <label>
+                  Your Role:{" "}
+                  <select name="role[]" multiple>
+                    <option value="leader">Leader</option>
+                    <option value="follower">Follower</option>
+                  </select>
+                </label>
+              </p>
+              <p>
+                <label>
+                  Message: <textarea name="message"></textarea>
+                </label>
+              </p>
+              <p>
+                <button type="submit">Send</button>
+              </p>
+            </form>
           </Col>
         </Row>
       </Container>
