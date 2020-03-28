43304ae560eca3bd6ef7801c5bf0524da74a564f
Nozomi Ishii <non.ankt@gmail.com>
2020-03-28 22:58:05 +0900

    created index page

--- src/canvas/sketch.js
+++ src/canvas/sketch.js
@@ -15,6 +15,7 @@ const sketch = (p) => {
       this.y += this.speed;
     }
   }
+
   p.mousePressed = () => {
     beam = new Beam(p.mouseX, p.mouseY, -3);
     beams.push(beam);
@@ -27,7 +28,12 @@ const sketch = (p) => {
     p.createCanvas(p.windowWidth, p.windowHeight * 0.7);
   };
 
-  let titleX = p.windowWidth * 0.5;
+  const title = {
+    name: "DANCE + CODE + ART = ME!!!",
+    x: p.windowWidth * 0.5,
+    y: p.windowHeight * 0.3,
+    height: 22,
+  };
   p.draw = () => {
     p.background(0);
     p.textSize(46);
@@ -35,12 +41,25 @@ const sketch = (p) => {
     p.textAlign(p.CENTER);
     p.text("👨🏻‍🚀", p.mouseX, p.mouseY);
 
-    p.textSize(22);
-    p.text("DANCE + CODE + ART = ME!!!", titleX, p.windowHeight * 0.3);
+    // p.rectMode(p.CENTER);
+    // p.rect(title.x, title.y, p.textWidth(title.name) / 2, title.height);
+
+    p.fill(255);
+    p.textSize(title.height);
+    p.text(title.name, title.x, title.y);
 
     for (beam of beams) {
       beam.shoot();
+      let dx = Math.abs(beam.x - title.x);
+      let dy = Math.abs(beam.y - title.y);
+      if (dx < p.textWidth(title.name) / 2 && dy < title.height) {
+        console.log("hit");
+        p.fill(p.random(255), p.random(255), p.random(255));
+        p.textSize(title.height);
+        p.text(title.name, title.x, title.y);
+      }
     }
+
     // p.noLoop();
   };
 };
