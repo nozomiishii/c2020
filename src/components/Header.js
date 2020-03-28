97c518010ce07c8e68b4d96133927c28e71942f2
Nozomi Ishii <non.ankt@gmail.com>
2020-03-29 05:03:00 +0900

    finish?!!"

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,35 +4,40 @@ import headerStyles from "../styles/components/header.module.scss";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+
+const Linklist = () => {
+  const links = ["about", "info", "codelab", "contact"];
+  return links.map((link) => (
+    <Link
+      to={`/${link}`}
+      key={link}
+      activeClassName={headerStyles.activeNavItem}
+    >
+      {link.toUpperCase()}
+    </Link>
+  ));
+};
+
 const Header = () => {
-  // const links = ["about", "info", "codelab", "contact"];
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log("hit");
+
+    const linklist = document.querySelector("#headerLinkList");
+    linklist.classList.toggle(headerStyles.show);
+  };
   return (
     <Container>
       <Row className={headerStyles.header}>
-        <Col md={5} className={headerStyles.title}>
+        <Col xs={7} md={5} className={headerStyles.title}>
           <Link to="/">
             <h1>NOZOMI ISHII</h1>
           </Link>
         </Col>
-        <Col md={7} className={headerStyles.menu}>
-          <div>
-            {/* {links.map((link) => (
-              <Link to={`/${link}`} activeClassName={headerStyles.activeNavItem}>
-                {link.toUpperCase()}
-              </Link>
-            ))} */}
-            <Link to="/about" activeClassName={headerStyles.activeNavItem}>
-              ABOUT
-            </Link>
-            <Link to="/info" activeClassName={headerStyles.activeNavItem}>
-              INFO
-            </Link>
-            <Link to="/codelab" activeClassName={headerStyles.activeNavItem}>
-              CODE LAB
-            </Link>
-            <Link to="/contact" activeClassName={headerStyles.activeNavItem}>
-              CONTACT
-            </Link>
+        <Col xs={5} md={7} className={headerStyles.menu}>
+          <button onClick={handleClick}>MENU</button>
+          <div id="headerLinkList">
+            <Linklist />
           </div>
         </Col>
       </Row>
