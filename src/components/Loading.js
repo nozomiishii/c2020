6522f27fb7edaf05782595fc00ad5803a31d1664
Nozomi Ishii <non.ankt@gmail.com>
2020-03-30 00:47:47 +0900

    create counter

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,15 +1,23 @@
 import React from "react";
 import classes from "../styles/components/loading.module.scss";
 
-const Loading = () => (
-  <div className={classes.loading}>
-    <h3>
-      <span className={classes.emoji} role="img" aria-label="img">
-        👨🏻‍🚀
-      </span>
-      LOADING...
-    </h3>
-  </div>
-);
+const Loading = () => {
+  const [counter, setCounter] = React.useState(0);
+  React.useEffect(() => {
+    counter < 99 && setTimeout(() => setCounter(counter + 1), 100);
+  }, [counter]);
+
+  return (
+    <div className={classes.loading}>
+      <h3>
+        <span className={classes.emoji} role="img" aria-label="img">
+          👨🏻‍🚀
+        </span>
+        LOADING...
+      </h3>
+      <p>{counter}%</p>
+    </div>
+  );
+};
 
 export default Loading;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,26 @@
 import React from "react";
 import Layout from "../components/Layout";
 import Head from "../components/Head";
-import sketch from "../canvas/sketch";
 import classes from "../styles/pages/index.module.scss";
-import Loadable from "react-loadable";
 import Loading from "../components/Loading";
+// import sketch from "../canvas/sketch";
+// import Loadable from "react-loadable";
 
 class IndexPage extends React.Component {
   render() {
     if (typeof window !== "undefined") {
-      const P5Wrapper = Loadable({
-        loader: () => import("react-p5-wrapper"),
-        loading: Loading,
-      });
+      // const P5Wrapper = Loadable({
+      //   loader: () => import("react-p5-wrapper"),
+      //   loading: Loading,
+      // });
 
       return (
         <div className={classes.index}>
           <Layout>
             <Head title="HOME" />
             <div className={classes.sketch}>
-              <P5Wrapper sketch={sketch} />
-              {/* <Loading /> */}
+              {/* <P5Wrapper sketch={sketch} /> */}
+              <Loading />
             </div>
           </Layout>
         </div>
