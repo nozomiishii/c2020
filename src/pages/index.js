aa3ff004198539124d823cb584daa31bc0d7f7c6
Nozomi Ishii <non.ankt@gmail.com>
2020-03-29 17:07:14 +0900

    done!

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,26 @@
 import React from "react";
 import Layout from "../components/Layout";
 import Head from "../components/Head";
-import sketch from "../canvas/sketch";
+// import sketch from "../canvas/sketch";
 import classes from "../styles/pages/index.module.scss";
-import Loadable from "react-loadable";
+// import Loadable from "react-loadable";
 import Loading from "../components/Loading";
 
 class IndexPage extends React.Component {
   render() {
     if (typeof window !== "undefined") {
-      const P5Wrapper = Loadable({
-        loader: () => import("react-p5-wrapper"),
-        loading: Loading,
-      });
+      // const P5Wrapper = Loadable({
+      //   loader: () => import("react-p5-wrapper"),
+      //   loading: Loading,
+      // });
 
       return (
         <div className={classes.index}>
           <Layout>
             <Head title="HOME" />
             <div className={classes.sketch}>
-              <P5Wrapper sketch={sketch} />
+              {/* <P5Wrapper sketch={sketch} /> */}
+              <Loading />
             </div>
           </Layout>
         </div>
