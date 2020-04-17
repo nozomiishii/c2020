f6ae3ce84d0e42ab48f44da9a844cd0341faf385
Nozomi Ishii <non.ankt@gmail.com>
2020-04-18 03:17:56 +0900

    added new page

--- src/pages/lists.js
+++ src/pages/lists.js
@@ -14,8 +14,19 @@ const ListsPage = () => {
   return (
     <Layout title="Lists">
       <p>個人的に今キテいるWebpages</p>
+      <h2>tutorials</h2>
       <div className={classes.lists}>
         <PageLinks data={tutorials} />
+      </div>
+
+      <h2>events</h2>
+      <div className={classes.lists}>
+        <PageLinks data={events} />
+      </div>
+
+      <h2>articles</h2>
+      <div className={classes.lists}>
+        <PageLinks data={articles} />
       </div>
     </Layout>
   );
