6fe0dd69e550d66450136c971e0480014439cc46
Nozomi Ishii <non.ankt@gmail.com>
2020-05-13 19:19:44 +0900

    sort

---
+++ cv.js
@@ -0,0 +1,19 @@
+const newSkill = "Passport.js JWT OAuth2.0 ";
+
+const oldSkills =
+  "Babel CSS Contentful ESLint Enzyme Express Firebase Gatsby.js GitHub GraphQL HTML Heroku Homebrew JavaScript Jest MangoDB Material-UI Node.js React SCSS TypeScript VSCode ml5.js npm p5.js webpack4 yarn";
+
+const skills = newSkill + oldSkills;
+console.log(skills);
+const sorter = (strSkills) => {
+  const arrSKills = strSkills.split(" ");
+  console.log(arrSKills);
+  // return arrSKills.sort((a, b) => b - a).join("<br/>");
+  return arrSKills.sort().join("<br/>");
+};
+
+console.log(sorter(skills));
+
+const cv = document.getElementById("cv");
+
+cv.innerHTML = sorter(skills);
